8de818b8744a29d21f9dd06b74d332b79a3e6cef
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-03-29 20:49:56 -0300

    feat: alternative Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import { ActivityIndicator } from "react-native";
+import { Text } from "../Text";
+import * as S from "./styles";
+
+interface Props {
+    children: string;
+    onPress(): void;
+    disabled?: boolean;
+    loading?: boolean;
+}
+
+export function Button(props: Props) {
+    const { children, loading = false, disabled = false, onPress } = props;
+
+    return (
+        <S.Container disabled={disabled} onPress={onPress}>
+            {loading ? (
+                <ActivityIndicator color="fff" />
+            ) : (
+                <Text weight="600" color="#fff">
+                    {children}
+                </Text>
+            )}
+        </S.Container>
+    );
+}
